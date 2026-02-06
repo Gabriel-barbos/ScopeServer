08fbc3116d1805b3b208c3deda4bbf4788401704
gabriel-barbos <gabriel.barbosa79@@outlook.com>
2026-02-06 14:17:04 -0300

    feat: client description flexibility

--- system/models/Client.js
+++ system/models/Client.js
@@ -4,8 +4,8 @@ import { getSystemDB } from "../../config/databases.js";
 const ClientSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    image: { type: [String], required: true },
-    description: { type: String, required: true },
+    image: { type: [String], required: false },
+    description: { type: String, required: false },
     type: { type: String, default: "padrão" },
   },
   { timestamps: true }
