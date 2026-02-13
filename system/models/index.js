481ad56048ce65d24e0b95e4039a8e0bdf5b3cff
gabriel-barbos <gabriel.barbosa79@@outlook.com>
2026-02-13 14:16:57 -0300

    fix: case import

--- system/models/index.js
+++ system/models/index.js
@@ -3,7 +3,7 @@ import getProductModel from "./Product.js";
 import getScheduleModel from "./Schedule.js";
 import getServiceModel from "./Service.js";
 import getUserModel from "./User.js";
-import getServiceLegacyModel from "./Servicelegacy.js";
+import getServiceLegacyModel from "./ServiceLegacy.js";
 
 let modelsInitialized = false;
 
